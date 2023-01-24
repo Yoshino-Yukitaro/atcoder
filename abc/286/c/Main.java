05c988702a18b15a416d4efdab4f63234f6bf6d4
Yoshino-Yukitaro <sushiki.az@gmail.com>
2023-01-24 22:28:42 +0900

    fix: abc286のc問題を復習した

--- abc/286/c/Main.java
+++ abc/286/c/Main.java
@@ -7,5 +7,15 @@ class Main {
         long A = sc.nextLong();
         long B = sc.nextLong();
         String S = sc.next();
+        long ans = 1000000000000000000L;
+        S += S;
+        for(int i = 0;i < N;i++){
+            long buf = i * A;
+            for(int j = 0;j < N/2;j++){
+                if(S.charAt(i+j) != S.charAt(N-1+i-j)) buf += B;
+            }
+            ans = Math.min(ans, buf);
+        }
+        System.out.println(ans);
     }
 }
