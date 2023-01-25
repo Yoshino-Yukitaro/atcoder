89217dbcb5bb254b7a3f6e1f9b9b032bf822861c
Yoshino-Yukitaro <sushiki.az@gmail.com>
2023-01-25 23:18:28 +0900

    fix: abc286のd問題を復習した

--- abc/286/d/Main.java
+++ abc/286/d/Main.java
@@ -3,72 +3,26 @@ import java.util.*;
 class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int N = sc.nextInt()-1;
         int X = sc.nextInt();
-        Map<Integer,Integer> map = new HashMap<>();
-        int[] dp = new int[N+1];
-        int[] count = new int[N];
-        int[] sum = new int[N];
-        int deps = 0;
-        int min_deps = 0; //現在0枚ではない深さ
-        dp[0] = X;
-        int[] buf = new int[N];
-        for(int i = 0;i < N;i++){
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            map.put(A,B);
-            buf[i] = A;
+        int[] A = new int[N+1];
+        int[] B = new int[N+1];
+        boolean[][] dp = new boolean[N+2][10001]; //A_i円硬貨をB_i枚まで用いた時、ピッタリのj円になり得るならtrue
+        for(int i = 0;i < N+1;i++){
+            A[i] = sc.nextInt();
+            B[i] = sc.nextInt();
         }
-        Arrays.sort(buf);
-        int max_value = buf[N-1];
-        int max_value_count = map.get(max_value);
-        int can_max_count = X / max_value;
-        count[N-1] = Math.min(max_value_count, can_max_count);
-        sum[0] = max_value * count[N-1];
-        deps = 1;
-        String ans = "No";
-        if(sum[0] == X){
-            min_deps = N;
-        }
-        while(min_deps < N){
-            if(count[N-1-min_deps] == 0){
-                min_deps++;
-                deps = min_deps;
-            }
-            if(deps == min_deps){
-                count[N-1-min_deps] -= 1;
-                sum[min_deps] = buf[N-1-min_deps] * count[N-1-min_deps];
-                deps = min_deps+1;
-            }else if(sum[deps] != 0){
-                if(count[N-1-deps] == 0){
-                    sum[deps] = 0;
-                    deps--;
-                }else if(deps == min_deps){
-                    count[N-1-min_deps] -= 1;
-                    sum[min_deps] = buf[N-1-min_deps] * count[N-1-min_deps];
-                    deps = min_deps+1;
-                }else{
-                    count[N-1-deps] -= 1;
-                    sum[deps] = sum[deps-1] + count[N-1-deps] * buf[N-1-deps];
-                    deps++;
-                }
-            }else{
-                int deps_max_value = buf[N-1-deps];
-                int deps_max_value_count = map.get(deps_max_value);
-                int can_deps_max_count = (X - sum[deps - 1]) / deps_max_value;
-                count[N-1-deps] = Math.min(deps_max_value_count, can_deps_max_count);
-                sum[deps] = sum[deps-1] + deps_max_value * count[N-1-deps];
-                if(sum[deps] == X){
-                    ans = "Yes";
-                    break;
-                }else if(deps == N-1){
-                    sum[deps] = 0;
-                    deps--;
-                }else{
-                    deps++;
+        dp[0][0] = true;
+        for(int i = 0;i < N+1;i++){ //A_iとB_iについて
+            for(int j = 0;j < X+1;j++){ //全ての金額についてdp[i][j]を求めたい
+                for(int k = 0;k < B[i]+1;k++){ //0<=k<=B_iで、利用可能な硬貨の枚数を全探索
+                    if(j>=A[i]*k){  //A_i円硬貨がk枚の時と比較したい金額jを比較する。jの方が小さいならcontinue
+                        if(dp[i][j-A[i]*k]) dp[i+1][j] = true; // A_i円硬貨がB_i枚以内でj-A[i]*k円になるなら、それ以上硬貨を出さなくても以降はtrue
+                    } 
                 }
             }
-        } 
+        }
+        String ans = dp[N+1][X] ? "Yes" : "No";
         System.out.println(ans);
     }
 }
